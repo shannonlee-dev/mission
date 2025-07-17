7d8d8452bbd8caa8cb40e883228906331c520aaf
shannonlee-dev <shannon.lee.dev@gmail.com>
2025-07-17 19:11:08 +0900

    upgrade problem_2

---
+++ 2025-selection-1/github-vscode.md
@@ -0,0 +1,19 @@
+# 패키지 목록을 최신화하고, gh를 설치합니다.
+sudo apt update
+sudo apt install gh -y
+gh auth login
+
+https://github.com/login/device
+
+# 홈 디렉토리(~) 아래에 dev 폴더를 만들고 이동합니다.
+mkdir -p ~/dev/codyssey
+cd ~/dev/codyssey
+
+git clone https://github.com/shannonlee-dev/ia-codyssey.git
+
+
+### push 하는 법
+
+git config --global user.name "shannonlee-dev"
+
+git config --global user.email "님의 GitHub 이메일 주소"
--- 2025-selection-1/problem_2/power_calculator.py
+++ 2025-selection-1/problem_2/power_calculator.py
@@ -1,34 +1,42 @@
+import sys
+
+def power(base:float, exponent:int) -> float:
+    if exponent==0:
+        return 1.0
+    
+    elif base==0:
+        if exponent <0:
+            raise ZeroDivisionError("0 cannot be raised to a negative exponent.")
+        else:
+            return 0.0    
+    
+    result = 1.0
+    
+    for _ in range(abs(exponent)):
+        result*=base
+    
+    if exponent < 0 :
+        result=1/result
+
+    return result
+
 def main()->None:
-    try: 
+    try:
         base=float(input("Enter number: "))
     except ValueError:
-        print('Invalid number input.')
-        return
+        print("Invalid number input.")
+        sys.exit(1)   
     try:
         exponent=int(input("Enter exponent: "))
-    except ValueError:
-        print('Invalid exponent input.')
-        return
-    result=1
-
-    if base==0:
-        if exponent >0:
-            result = 0
-        elif exponent <0:
-            print('not defined')
-            return
-        elif exponent == 0:
-            result=1
-    else:
-        for _ in range(abs(exponent)):
-            result*=base
-
-        if exponent<0:  
-            result=1.0/result
-
-
-    print(f"Result: {result}")
-
+    except ValueError :
+        print("Invalid exponent input.")
+        sys.exit(1)
+    
+    try:
+        print(f"Result: {power(base,exponent)}")
+    except ZeroDivisionError as e:
+        print("Error",e)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
