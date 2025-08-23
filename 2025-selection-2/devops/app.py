03da5f2fae698d93511d47765f0b5648387aebfc
shannonlee-dev <shannon.lee.dev@gmail.com>
2025-08-23 13:26:24 +0900

    .

--- 2025-selection-2/devops/app.py
+++ 2025-selection-2/devops/app.py
@@ -10,6 +10,10 @@ def home():
     else:
         hostname = ' '
     return render_template("menu.html",computername=hostname)
+
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8800, debug = True)
