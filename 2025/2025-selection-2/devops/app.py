95779f313896aa69537531834b7131b972d922e2
shannonlee-dev <shannon.lee.dev@gmail.com>
2026-05-24 10:46:33 +0900

    feat(2025-selection-2): add devops tea menu app
    Absorbs: 5cf8a87..2d6f68a including merge, test, and cleanup commits.\n\nAdds the Flask tea menu app, templates, and David image asset.

---
+++ 2025/2025-selection-2/devops/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__)
+
+@app.route("/menu")
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template("menu.html",computername=hostname)
+
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8800, debug = True)
+
+#/menu  <---> home함수 <-->gi html    합체
+
+
+꾸잉222222?
+꾸잉?
